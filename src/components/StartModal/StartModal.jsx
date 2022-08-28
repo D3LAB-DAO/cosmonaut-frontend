e0ca01dd15fff0075ceec7c1f00d93bddf6ea5fe
추교현 <scottchoo@chugyohyeon-ui-MacBookPro.local>
2022-08-28 17:59:20 +0900

    fix: start modal ui/ux

--- src/components/StartModal/StartModal.jsx
+++ src/components/StartModal/StartModal.jsx
@@ -8,7 +8,7 @@ import { usePostInitial } from "../../libs/api/postInitial";
 import classNames from "classnames";
 
 const Navigate = tw.div`flex flex-wrap mt-5 mx-auto justify-center gap-3 rounded-3xl`;
-const Button = tw.button`bg-white  hover:bg-yellow-100 focus:bg-yellow-500 focus:outline-none hover:z-10 focus:z-10 focus:ring-4 focus:ring-inset focus:ring-orange-400 active:bg-yellow-500 xl:h-44 md:h-36 h-32 flex items-center justify-center w-2/5 md:w-1/5 xl:w-1/6 rounded-xl border-2 border-indigo-900 ease-in-out duration-300`;
+const Button = tw.button`bg-white h-40 flex items-center justify-center w-2/5 md:w-1/5 xl:w-1/8 rounded-xl border-2 border-indigo-900 ease-in-out duration-300`;
 const ChNumber = tw.h2`h-1/2 w-full px-1 xl:pb-4 md:pb-3 pb-3 block md:text-base xl:text-lg text-xs font-heading text-indigo-900`;
 const ChTitle = tw.h3`h-1/2 w-full px-1 xl:pt-4 md:pt-3 pt-2 xl:text-sm text-center text-xs font-mono text-indigo-900`;
 
@@ -23,7 +23,8 @@ function StartModal() {
   const engInfo = useRecoilValue(lessonEngInfo);
   const chInfo = useRecoilValue(chapterInfos);
   const [key, setKey] = useState(chID);
-  console.log(key);
+  console.log(typeof key);
+  console.log(typeof chID);
 
   useEffect(() => {
     if (lessonID === "1" && chID === "6") {
@@ -40,14 +41,33 @@ function StartModal() {
   }, []);
 
   const closeModal = async () => {
-    if (key < chID || key === "undefined") {
-      alert("Wrong Access");
-    } else {
-      await initFetch();
-      const modal = document.querySelectorAll("#modal");
-      modal[0].classList.add("hidden");
-    }
+    await initFetch();
+    const modal = document.querySelectorAll("#modal");
+    modal[0].classList.add("hidden");
   };
+  let chState;
+
+  if (key === chID) {
+  } else if (key > chID) {
+    console.log("key > chID");
+    chState = (
+      <>
+        <h3 class="text-center mb-2 md:text-lg mt-8 text-xs mx-auto text-red-500 block w-full font-extrabold underline">
+          You don't have access to this chapter yet
+        </h3>
+      </>
+    );
+  } else if (key < chID) {
+    console.log("key < chID");
+    chState = (
+      <>
+        <h3 class="text-center mb-2 md:text-lg mt-8 text-xs mx-auto text-orange-400 block w-full font-extrabold underline">
+          You've already done this chapter
+        </h3>
+      </>
+    );
+  }
+
   return (
     <>
       <div
@@ -63,23 +83,26 @@ function StartModal() {
               {engInfo[lessonID]?.title}
             </h4>
             <Navigate>
-              {chInfo[lessonID].map((e, index) => {
+              {chInfo[lessonID].map((e) => {
                 return (
                   <Button
-                    onClick={async () => {
-                      await setKey(e.id);
+                    onClick={() => {
+                      setKey(e.id);
                     }}
-                    disabled={chID < String(e.id)}
+                    // disabled={chID < String(e.id)}
                     className={classNames(
-                      "xl:h-44 md:h-36 h-32 flex items-center justify-center w-2/5 md:w-1/5 xl:w-1/6 rounded-xl border-2 border-indigo-900",
-                      chID >= String(e.id)
-                        ? "hover:z-10 hover:bg-yellow-100 bg-white ease-in-out duration-300 focus:bg-yellow-500 focus:outline-none focus:z-10 focus:ring-4 focus:ring-inset focus:ring-orange-400 active:bg-yellow-500"
-                        : "hover:bg-gray-400 active:bg-gray-400 bg-gray-400"
+                      chID < String(e.id)
+                        ? "hover:z-10 hover:bg-yellow-100 bg-white"
+                        : chID > String(e.id)
+                        ? "bg-green-400 hover:bg-yellow-100 focus:bg-yellow-500 focus:outline-none hover:z-10 focus:z-10 focus:ring-4 focus:ring-inset focus:ring-orange-400 active:bg-yellow-500"
+                        : chID === String(e.id)
+                        ? "ring-4 ring-inset ring-orange-400 bg-yellow-500"
+                        : "hover:z-10 hover:bg-yellow-100 bg-white"
                     )}
                   >
                     <div
                       id="btn"
-                      class="block w-full h-full p-4 items-center divide-y-2 divide-indigo-900 justify-center"
+                      class="block w-full h-full p-6 items-center divide-y-2 divide-indigo-900 justify-center"
                     >
                       <ChNumber>Chapter {e.id}</ChNumber>
                       <ChTitle>{e.title}</ChTitle>
@@ -88,12 +111,16 @@ function StartModal() {
                 );
               })}
             </Navigate>
-            <button
-              onClick={closeModal}
-              class="animate-moveUtoD block mt-12 mb-72 mx-auto md:mb-4 text-center text-lg border-3 transition duration-200 rounded-full py-2 px-8 bg-gradient-to-r to-orange-400 from-yellow-500 font-heading text-indigo-900 hover:from-green-500 border-indigo-900 hover:border-white hover:to-blue-500 hover:text-white"
-            >
-              Get Started
-            </button>
+            {String(key) === chID ? (
+              <button
+                onClick={closeModal}
+                class="animate-moveUtoD block mt-6 mb-72 mx-auto md:mb-4 text-center text-lg border-3  transition duration-200 rounded-full py-2 px-8 bg-gradient-to-r to-orange-400 from-yellow-500 font-heading text-indigo-900 hover:from-green-500 border-indigo-900 hover:border-white hover:to-blue-500 hover:text-white"
+              >
+                Get Started
+              </button>
+            ) : (
+              chState
+            )}
           </div>
         </div>
       </div>
